3e4b06ea0e5fe2225c55ba720643fb99fd09e3f4
Jeffrey Klardie <mail@jeffreyklardie.nl>
2013-02-09 00:05:34 +0100

    Implemented Floyd-Warshall algorithm with parent tracking, print_matrix() and free_matrix(), and added runtime measuring

--- code/asp/my-seq.c
+++ code/asp/my-seq.c
@@ -9,22 +9,80 @@
 #define MAX_DISTANCE 256    // max distance between places randomly generated
 
 /**
+ * Apply the Floyd-Warshall algorithm to the adjacency matrix,
+ * and keep track of paths in the parent matrix.
+ */
+void floyd_warshall(int ***matrix, int ***parent, int n){
+    int i, j, k;
+    int new_dist;
+
+    for(k=0; k<n; k++){
+        for(i=0; i<n; i++){
+            if(i != k){
+                for(j=0; j<n; j++){
+                    new_dist = (*matrix)[i][k] + (*matrix)[k][j];
+                    if(new_dist < (*matrix)[i][j]){
+                        (*matrix)[i][j] = new_dist;
+
+                        // we just added a new node to the path between i and j (k),
+                        // so add k as the parent of j. So to go from i to j, k will
+                        // be visited directly before j.
+                        (*parent)[i][j] = (*parent)[k][j];
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+/**
+ * Print a given n x n matrix
+ */
+void print_matrix(int **matrix, int n){
+    int i, j;
+
+    for(i=0; i<n; i++){
+        for(j=0; j<n; j++){
+            printf("%2d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/**
+ * Free all memory used by the n x n matrix
+ */
+void free_matrix(int **matrix, int n){
+    int i;
+
+    for(i=0; i<n; i++){
+        free(matrix[i]);
+    }
+
+    free(matrix);
+}
+
+/**
  * Initialize a random adjacency matrix of size n x n.
  */
-void init_random_adjacency_matrix(int n, int *num_edges, int ***matrix, int oriented){
+void init_random_adjacency_matrix(int n, int *num_edges, int ***matrix, int ***parent_matrix, int oriented){
     int **am;   // adjacency matrix
+    int **pm;   // parent matrix
     int i, j, m = n * n;
 
     am = (int **) malloc(n * sizeof(int *));
-    if(am == (int **)0){
-        fprintf(stderr, "Error while allocating memory for adjacency matrix\n");
+    pm = (int **) malloc(n * sizeof(int *));
+    if(am == (int **)0 || pm == (int **)0){
+        fprintf(stderr, "Error while allocating memory for adjacency matrix or parent matrix\n");
         exit(EXIT_FAILURE);
     }
 
     for(i=0; i<n; i++){
         am[i] = (int *) malloc(n * sizeof(int));
-        if(am[i] == (int *)0){
-            fprintf(stderr, "Error while allocating memory for adjacency matrix\n");
+        pm[i] = (int *) malloc(n * sizeof(int));
+        if(am[i] == (int *)0 || pm[i] == (int *)0){
+            fprintf(stderr, "Error while allocating memory for adjacency matrix or parent matrix\n");
             exit(EXIT_FAILURE);
         }
 
@@ -43,10 +101,18 @@ void init_random_adjacency_matrix(int n, int *num_edges, int ***matrix, int orie
 
             if(am[i][j] == MAX_DISTANCE) m--;
             if(am[j][i] == MAX_DISTANCE) m--;
+
+            // store parents, used to restore path later on
+            if(i == j || am[i][j] == MAX_DISTANCE){
+                pm[i][j] = -1;
+            } else {
+                pm[i][j] = i;
+            }
         }
     }
 
     *matrix = am;
+    *parent_matrix = pm;
     *num_edges = m;
 }
 
@@ -61,8 +127,9 @@ void init_random_adjacency_matrix(int n, int *num_edges, int ***matrix, int orie
  *  - first line: [num vertices] [num edges] [oriented (0/1)]
  *  - following [num edges] lines: [source] [destination] [distance]
  */
-int read_adjacency_matrix(char *filename, int *num_vertices, int *num_edges, int ***matrix, int *oriented){
+int read_adjacency_matrix(char *filename, int *num_vertices, int *num_edges, int ***matrix, int ***parent_matrix, int *oriented){
     int **am;
+    int **pm;
     int i, j;
     int n;
     int source, dest, dist;
@@ -82,15 +149,17 @@ int read_adjacency_matrix(char *filename, int *num_vertices, int *num_edges, int
 
     n = *num_vertices;
     am = (int **) malloc(n * sizeof(int *));
-    if(am == (int **)0){
-        fprintf(stderr, "Error while allocating memory for adjacency matrix\n");
+    pm = (int **) malloc(n * sizeof(int *));
+    if(am == (int **)0 || pm == (int **)0){
+        fprintf(stderr, "Error while allocating memory for adjacency matrix or parent matrix\n");
         exit(EXIT_FAILURE);
     }
 
     for(i=0; i<n; i++){
         am[i] = (int *) malloc(n * sizeof(int));
-        if(am[i] == (int *)0){
-            fprintf(stderr, "Error while allocating memory for adjacency matrix\n");
+        pm[i] = (int *) malloc(n * sizeof(int));
+        if(am[i] == (int *)0 || pm[i] == (int *)0){
+            fprintf(stderr, "Error while allocating memory for adjacency matrix or parent matrix\n");
             exit(EXIT_FAILURE);
         }
 
@@ -102,18 +171,28 @@ int read_adjacency_matrix(char *filename, int *num_vertices, int *num_edges, int
 
     while(!feof(fp)){
         fscanf(fp, "%d %d %d \n", &source, &dest, &dist);
+        i = source-1;
+        j = dest-1;
+
         if(!(*oriented)){
             // distance i to j is the same as j to i
-            if(am[source-1][dest-1] < MAX_DISTANCE){
+            if(am[i][j] < MAX_DISTANCE){
                 // verify that we did not set this distance yet.
                 // if we did, we increment the number of bad edges, and ignore the distance
                 num_bad_edges++;
             } else {
-                am[source-1][dest-1] = dist;
-                am[dest-1][source-1] = dist;
+                am[i][j] = dist;
+                am[j][i] = dist;
             }
         } else {
-            am[source-1][dest-1] = dist;
+            am[i][j] = dist;
+        }
+
+        // store parents, used to restore path later on
+        if(i == j || am[i][j] == MAX_DISTANCE){
+            pm[i][j] = -1;
+        } else {
+            pm[i][j] = i;
         }
     }
 
@@ -129,6 +208,7 @@ int read_adjacency_matrix(char *filename, int *num_vertices, int *num_edges, int
     #endif
 
     *matrix = am;
+    *parent_matrix = pm;
 
     return num_bad_edges;
 }
@@ -148,9 +228,12 @@ int main(int argc, char **argv){
     int print = 0;          // print resulting adjacency matrix?
     int oriented = 0;       // whether or not the random matrix is oriented
     int **matrix;           // adjacency matrix
+    int **parent_matrix;    // matrix used to restore paths
     int num_bad_edges = 0;  // number of bad edges in file
     char filename[100];     // filename if given as param
 
+    struct timeval start, end;
+    double time;
 
 
     // print usage
@@ -174,13 +257,37 @@ int main(int argc, char **argv){
 
     if(num_vertices > 0){
         // init random adjacency matrix for testing
-        init_random_adjacency_matrix(num_vertices, &num_edges, &matrix, oriented);
+        init_random_adjacency_matrix(num_vertices, &num_edges, &matrix, &parent_matrix, oriented);
     } else {
         // generate adjacency matrix from file
-        num_bad_edges = read_adjacency_matrix(filename, &num_vertices, &num_edges, &matrix, &oriented);
+        num_bad_edges = read_adjacency_matrix(filename, &num_vertices, &num_edges, &matrix, &parent_matrix, &oriented);
     }
 
     fprintf(stderr, "Running ASP with %d rows and %d edges (%d are bad)\n", num_vertices, num_edges, num_bad_edges);
+
+    if(gettimeofday(&start, 0) != 0){
+        fprintf(stderr, "Error starting timer\n");
+        exit(EXIT_FAILURE);
+    }
+
+    floyd_warshall(&matrix, &parent_matrix, num_vertices);
+
+    if(gettimeofday(&end, 0) != 0){
+        fprintf(stderr, "Error stopping timer\n");
+        exit(EXIT_FAILURE);
+    }
+
+    time = (end.tv_sec + end.tv_usec / 1000000.0) -
+            (start.tv_sec + start.tv_usec / 1000000.0);
+
+    fprintf(stderr, "ASP took %10.3f seconds\n", time);
+
+    if(print){
+        print_matrix(matrix, num_vertices);
+    }
+
+    free_matrix(matrix, num_vertices);
+    free_matrix(parent_matrix, num_vertices);
 
     return 0;
 }
